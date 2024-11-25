cbf787da8bd40518a5a98de16cfe95c90d33d136
Peter Abbasi <poa0@icloud.com>
2024-11-24 19:47:30 -0600

    Update ligaturize.py

--- ligaturize.py
+++ ligaturize.py
@@ -24,16 +24,21 @@ FiraCode Copyright (c) 2015 by Nikita Prokopov'''
 def get_ligature_source(fontname):
     # Become case-insensitive
     fontname = fontname.lower()
-    for weight in ['Bold', 'Retina', 'Medium', 'Regular', 'Light']:
+    for weight in ['Bold', 'SemiBold', 'Retina', 'Medium', 'Regular', 'Light']:
         if fontname.endswith('-' + weight.lower()):
             # Exact match for one of the Fira Code weights
-            return 'fonts/fira/distr/otf/FiraCode-%s.otf' % weight
-
+            return 'lig/fonts/fira/distr/otf/FiraCode-%s.otf' % weight
     # No exact match. Guess that we want 'Bold' if the font name has 'bold' or
     # 'heavy' in it, and 'Regular' otherwise.
+    if 'light' in fontname:
+        return 'lig/fonts/fira/distr/otf/FiraCode-Light.otf'
+    if 'medium' in fontname:
+        return 'lig/fonts/fira/distr/otf/FiraCode-Medium.otf'
+    if 'semibold' in fontname:
+        return 'lig/fonts/fira/distr/otf/FiraCode-SemiBold.otf'
     if 'bold' in fontname or 'heavy' in fontname:
-        return 'fonts/fira/distr/otf/FiraCode-Bold.otf'
-    return 'fonts/fira/distr/otf/FiraCode-Regular.otf'
+        return 'lig/fonts/fira/distr/otf/FiraCode-Bold.otf'
+    return 'lig/fonts/fira/distr/otf/FiraCode-Regular.otf'
 
 class LigatureCreator(object):
 
